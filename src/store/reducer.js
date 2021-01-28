ffd955c37cf2a26b04c11938c2d40daea0b776b3
nitish2202 <nitish.2202@gmail.com>
2021-01-28 23:50:39 +0530

    working with first store, action, reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,28 @@
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+        case 'ADD-VALUE':
+            return {
+                counter: state.counter + action.value
+            }
+        case 'SUBTRACT-VALUE':
+            return {
+                counter: state.counter - action.value
+            }
+        default:
+            return state;
+    }
+};
+
+export default reducer;
