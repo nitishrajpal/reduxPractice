0906ae473e6772c1151dedb9ab0348a546eacdd2
nitish2202 <nitish.2202@gmail.com>
2021-01-30 13:53:04 +0530

    dispatching action to delete item immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -21,7 +21,7 @@ class Counter extends Component {
                 <button onClick={this.props.onStoreResult}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(strResult => (
-                        <li key={strResult.id} onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                        <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
                     ))}
                 </ul>
             </div>
@@ -43,7 +43,7 @@ const mapDispatchToProps = dispatch => {
         addValue: () => dispatch({type: 'ADD-VALUE', value: 5}),
         subtractValue: () => dispatch({type: 'SUBTRACT-VALUE', value: 5}),
         onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
-        onDeleteResult: () => dispatch({ type: 'DELETE_RESULT' })
+        onDeleteResult: (id) => dispatch({ type: 'DELETE_RESULT', resultElId: id })
     };
 };
 
